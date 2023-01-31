c3334c03ced0347077b8b7196585b8a9840bc4bc
WinkelCode <29005772+WinkelCode@users.noreply.github.com>
2023-01-31 08:26:20 +0000

    Packaging Tools

---
+++ packaging/packagetool.sh
@@ -0,0 +1,155 @@
+#!/usr/bin/env bash
+set -e
+
+# Get information
+# ---------------
+software_name="it87"
+current_date="$(date +%Y%m%d)"
+
+origin_url="$(git remote get-url origin 2>/dev/null || printf 'unknown')"
+[ "$origin_url" != 'unknown' ] && origin_name="${origin_url##*/}"
+[ "$origin_url" != 'unknown' ] && origin_owner="${origin_url%/*}"; origin_owner="${origin_owner##*/}"
+
+current_commit="$(git rev-parse HEAD 2>/dev/null || printf 'unknown')"
+[ "$current_commit" == 'unknown' ] && current_commit="${current_date}"
+
+current_commit_date="$(git show -s --format=%cd --date=short "${current_commit}" 2>/dev/null || printf 'unknown')"
+[ "$current_commit_date" == 'unknown' ] && current_commit_date="${current_date}"
+
+generic_version="0~${origin_owner}^${current_date}git${current_commit:0:7}"
+
+printf '%s\n' "Determined the following values:"
+printf '\t%s:%s\n' \
+	"software_name" "$software_name" \
+	"origin_name" "$origin_name" \
+	"origin_url" "$origin_url" \
+	"origin_owner" "$origin_owner" \
+	"current_commit" "$current_commit" \
+	"current_commit_date" "$current_commit_date" \
+	"current_date" "$current_date" \
+	"generic_version" "$generic_version" 
+
+# Functions
+# ---------
+
+build_apk() {
+	mkdir -p "${temp_dir}/"{APKBUILD/src,packages}
+	build_overrides=(
+		"source_modname=\"${software_name}\""
+		"repo_name=\"${origin_name}\""
+		"repo_owner=\"${origin_owner}\""
+		"repo_commit=\"${current_commit}\""
+		"repo_commit_date=\"${current_commit_date}\""
+		"package_timestamp=\"${current_date}\""
+	)
+	for build_file in "./packaging/apk-akms/"{APKBUILD,AKMBUILD}; do
+		build_file_target="${temp_dir}/APKBUILD/${build_file##*/}"
+		printf '%s\n' "${build_overrides[@]}" >"${build_file_target}"
+		cat "${build_file}" >>"${build_file_target}"
+	done
+	tar -czvf "${temp_dir}/APKBUILD/${origin_name}.tar.gz" "../${PWD##*/}"
+	echo "$(cat "./packaging/apk-akms/Containerfile")" | ${container_runtime} build -t ${software_name}-apk-builder -
+	container_mounts=(
+		"--mount type=bind,source=${temp_dir}/APKBUILD,target=/APKBUILD"
+		"--mount type=bind,source=${temp_dir}/packages,target=/root/packages"
+	)
+	run_command="abuild-keygen -a -n && abuild -F checksum && abuild -F srcpkg && abuild -F"
+	${container_runtime} run --rm ${container_mounts[@]} ${software_name}-apk-builder ash -c "${run_command}" || { echo "Error: Failed to build APKs"; exit 1; }
+	tree "${temp_dir}" 2>/dev/null || ls -R "${temp_dir}"
+	mkdir -p ".release/"
+	cp "${temp_dir}/packages/"*/*.apk ".release/"
+}
+
+build_deb() {
+	printf '%s\n' "NOT IMPLEMENTED YET"
+}
+
+build_rpm() {
+	mkdir -p "${temp_dir}/"{SOURCES,SPECS,RPMS,SRPMS} # Create shared build directories in temp dir
+	spec_overrides=(
+		"%global source_modname ${software_name}"
+		"%global repo_name ${origin_name}"
+		"%global repo_owner ${origin_owner}"
+		"%global repo_commit ${current_commit}"
+		"%global package_timestamp ${current_date}"
+	)
+	for spec_file in "./packaging/rpm-akmod/"*.spec; do # Write overrides, then insert the original spec file
+		spec_file_target="${temp_dir}/SPECS/${spec_file##*/}"
+		printf '%s\n' "${spec_overrides[@]}" >"${spec_file_target}"
+		cat "${spec_file}" >>"${spec_file_target}"
+	done
+	tar -czvf "${temp_dir}/SOURCES/${origin_name}.tar.gz" "../${PWD##*/}" # The spec files expect the sources in a subdirectory of the archive (as with GitHub tarballs)
+	echo "$(cat "./packaging/rpm-akmod/Containerfile")" | ${container_runtime} build -t ${software_name}-rpm-builder - # We pipe the Containerfile for Docker compatibility as it expects a "Dockerfile" file
+	container_mounts=(
+		"--mount type=bind,source=${temp_dir}/SOURCES,target=/root/rpmbuild/SOURCES"
+		"--mount type=bind,source=${temp_dir}/SPECS,target=/root/rpmbuild/SPECS"
+		"--mount type=bind,source=${temp_dir}/RPMS,target=/root/rpmbuild/RPMS"
+		"--mount type=bind,source=${temp_dir}/SRPMS,target=/root/rpmbuild/SRPMS"
+	)
+	${container_runtime} run --rm ${container_mounts[@]} ${software_name}-rpm-builder bash -c "rpmbuild -ba /root/rpmbuild/SPECS/*.spec" || { echo "Error: Failed to build RPMs"; exit 1; }
+	tree "${temp_dir}" 2>/dev/null || ls -R "${temp_dir}" # We print the contents of the temp dir for basic debugging
+	mkdir -p ".release/"{SRPMS,RPMS}
+	cp "${temp_dir}/SRPMS/"*.src.rpm ".release/SRPMS/"
+	cp "${temp_dir}/RPMS/"*/*.rpm ".release/RPMS/"
+}
+
+build_generic_dkms() {
+	printf '%s\n' "NOT IMPLEMENTED YET"
+}
+
+# Main
+# ----
+
+help="Usage: $0 [docker (experimental)|podman (recommended)] [apk|deb|rpm|tar]"
+
+rm -rf ".release" # Remove old output directory
+temp_dir="$(mktemp -t --directory ${software_name}_tmp.XXXXXXXXXX)"
+trap "rm -rf ${temp_dir}" EXIT # Remove temp dir on exit, uncomment for debugging
+
+case "$1" in
+	docker|d|dock)
+		if ! command -v docker &>/dev/null; then
+			printf '%s\n' "Error: Docker is not installed."
+			exit 1
+		fi
+		printf '%s\n' "Warning: Docker support is experimental, Podman is recommended."
+		read -p "Continue? [y/N] "
+		if [[ ! "$REPLY" =~ ^[Yy] ]]; then
+			printf '%s\n' "Aborting..."
+			exit 1
+		fi
+		container_runtime="docker"
+		;;
+	podman|p|pod)
+		if ! command -v podman &>/dev/null; then
+			printf '%s\n' "Error: Podman is not installed."
+			exit 1
+		fi
+		container_runtime="podman"
+		;;
+	*)
+		printf '%s\n' "$help"
+		exit 1
+		;;
+esac
+
+case "$2" in
+	apk)
+		build_apk
+		;;
+	deb)
+		build_deb
+		;;
+	rpm)
+		build_rpm
+		;;
+	generic_dkms)
+		build_generic_dkms
+		;;
+	*)
+		printf '%s\n' "$help"
+		exit 1
+		;;
+esac
+
+echo "Done."
---
+++ packaging/rpm-akmod/it87-oot.spec
@@ -0,0 +1,53 @@
+# Placeholder information will be used if not inserted above (e.g. by a build wrapper)
+%{!?source_modname: %global source_modname it87} # Module name as it appears in the source code
+%{!?repo_name: %global repo_name it87}
+%{!?repo_owner: %global repo_owner frankcrawford}
+%{!?repo_commit: %global repo_commit 77abcbe0c49d7d8dc4530dcf51cecb40ef39f49a}
+%{!?package_timestamp:%global package_timestamp %{lua:print(os.date('!%Y%m%d'))}}
+# Placeholder information end
+
+%define commit_short() %{lua:print(string.sub(rpm.expand('%repo_commit'),1,arg[1]))}
+
+
+Name:           %{source_modname}-oot
+Version:        %{!?version_override:0^%{package_timestamp}.git%{commit_short 7}}%{?version_override:%{version_override}} 
+Release:        1%{?dist}
+Summary:        Userland package for the out-of-tree version of the "%{source_modname}" module forked by "%{repo_owner}".
+License:        GPLv2+
+
+URL:            https://github.com/%{repo_owner}/%{repo_name}
+Source0:        %{url}/tarball/%{repo_commit}/%{repo_name}.tar.gz
+
+Requires:       %{name}-kmod >= %{version}
+Provides:       %{name}-kmod-common = %{version}
+
+BuildArch:      noarch
+
+%description
+Out-of-tree fork of the %{source_modname} kernel module with support for more chips. This is the userland package.
+
+%prep
+%setup -q -c
+
+%global source_dirname %(tar -tzf %{SOURCE0} | head -n 1 | head -c -2)
+if [ ! -f "%{source_dirname}/Makefile" ]; then
+	echo "ERROR: Makefile not found in source archive, we expect the archive to contain a single directory with the source code."
+	exit 1
+fi
+
+%build
+printf '%s' "override %{source_modname} * extra/%{name}" >"depmod_%{name}.conf"
+printf '%s' "%{source_modname}" >"modload_%{name}.conf"
+
+%install
+install -D -m 0644 "depmod_%{name}.conf" "%{buildroot}%{_prefix}/lib/depmod.d/%{name}.conf"
+install -D -m 0644 "modload_%{name}.conf" "%{buildroot}%{_prefix}/lib/modules-load.d/%{name}.conf"
+
+%files
+%doc %{source_dirname}/README %{source_dirname}/ISSUES
+%license %{source_dirname}/LICENSE
+%{_prefix}/lib/depmod.d/%{name}.conf
+%{_prefix}/lib/modules-load.d/%{name}.conf
+
+%changelog
+# Nothing so far
